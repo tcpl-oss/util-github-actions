5b8fc73181956aec7b69c0c40cbef5bc08243dd9
Steve Hayes <steven.hayes@gmail.com>
2019-04-24 23:27:20 +0100

    blah

--- ssh-setup/entrypoint.sh
+++ ssh-setup/entrypoint.sh
@@ -9,11 +9,13 @@ touch "$SSH_PATH/known_hosts"
 gpg -q --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" -o "$SSH_PATH/id_rsa" id_rsa.gpg
 
 cp "id_rsa.pub" $SSH_PATH
+cp /config $SSH_PATH
 
 chmod 700 "$SSH_PATH"
 chmod 600 "$SSH_PATH/known_hosts"
 chmod 600 "$SSH_PATH/id_rsa"
 chmod 600 "$SSH_PATH/id_rsa.pub"
+chmod 600 "$SSH_PATH/config"
 
 eval $(ssh-agent)
 ssh-add "$SSH_PATH/id_rsa"
